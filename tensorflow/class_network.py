7d31bb571fde7cde424ad0321d069c4e542bb4fe
ToHanwei <hanwei@shanghaitech.edu.cn>
2019-01-15 15:59:32 +0800

    2019-01-15_01

--- tensorflow/class_network.py
+++ tensorflow/class_network.py
@@ -2,7 +2,18 @@ import numpy as np
 import random
 
 class Network(object):
+
 	def __init__(self, sizes):
+		"""The list ``sizes`` contains the number of neurons in the
+        respective layers of the network.  For example, if the list
+        was [2, 3, 1] then it would be a three-layer network, with the
+        first layer containing 2 neurons, the second layer 3 neurons,
+        and the third layer 1 neuron.  The biases and weights for the
+        network are initialized randomly, using a Gaussian
+        distribution with mean 0, and variance 1.  Note that the first
+        layer is assumed to be an input layer, and by convention we
+        won't set any biases for those neurons, since biases are only
+        ever used in computing the outputs from later layers."""
 		self.num_layers = len(sizes)
 		self.sizes = sizes
 		self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
@@ -56,9 +67,24 @@ class Network(object):
 			delta_nabla_b, delta_nabla_w = self.backprop(x, y)
 			nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
 			nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
-		self.weighs = [w-(eta/len(mini_batch))*dw
+		self.weighs = [w-(eta/len(mini_batch))*nw
 						for w, nw in zip(self.weighs, nabla_w)]
 		self.biases = [b-(eta/len(mini_batch))*nb
 						for b, nb in zip(self.biases, nabla_b)]
+	
+	def backprop(self, x, y):
+		"""Return a tuple ``(nabla_b, nabla_w)`` representing the
+        gradient for the cost function C_x.  ``nabla_b`` and
+        ``nabla_w`` are layer-by-layer lists of numpy arrays, similar
+        to ``self.biases`` and ``self.weights``."""
+		nabla_b = [np.zeros(b.shape) for b in self.biases]
+		nabla_w = [np.zeros(w.shape) for w in self.weighs]
+		#feedforward
+		activation = x
+		activations = [x]
+		zs = []
+		for b, w in zip(self.biases, self.weighs):
+			z = np.dot(w, activation) + b
+		
 	
 
